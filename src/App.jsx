bbf501b740fda06d9f11e6a3fef429b6fe699a1a
Shujaat Ahmed <ahmed.shujaat555@gmail.com>
2024-03-05 01:24:16 +0500

    carasouels

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Task2 from './assets/components/tasktwo'
+
+ const App = () => {
+  return (
+    <div><Task2 /></div>
+  )
+}
+export default App
---
+++ src/assets/components/tasktwo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+ const tasktwo = () => {
+  return (
+    <div className='max-w-[90rem] h-screen m-auto bg-stone-800 flex justify-center'>
+        <div className=''>
+            <h1 className='font-[600] text-[2rem] w-[30rem] text-white'>Your Go-To Digital Marketing Agency in Dubai</h1>
+        </div>
+        <div className='border-box w-[40rem] h-[28rem] bg-stone-700 space-y-4 mt-[10rem] '>
+            <div className='p-5'>
+            <h1 className='text-white font-[600] m-auto'>Esclate your digital growth with our AI-enabled Digital Marketing Services</h1>
+            <p className='text-[#fff] font-[300] text-[1.125em]'>We use every digital platform to ensure that your audience is accurately targeted. 
+                Our personalized and creative digital marketing strategies can promote your brand 
+                and establish a strong place for your business in the market.</p>
+
+                <p className='text-[#fff] font-[300] text-[1.125em] mt-[10rem]'>Our team of digital marketing experts has a proven track record of helping brands thrive in the digital landscape. 
+                    We curate and implement customer-centric digital marketing campaigns to provide our clients with extensive market 
+                    reach and drive more leads.</p>
+
+                    </div>
+        </div>
+    </div>
+  )
+}
+export default tasktwo
